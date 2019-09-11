363a259d9facf28a09cda52513f3ebb5bc6221da
Vladislav <vmos1999@mail.ru>
2019-09-11 13:55:28 +0300

    repository created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='test-task',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Neuromation test task',
+    author='Username',
+)
